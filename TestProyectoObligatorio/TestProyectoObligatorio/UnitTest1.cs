42f89e5b4615773d202280eac82234f636c408fe
Ulifer853 <ufernand853@gmail.com>
2018-09-17 08:39:06 -0300

    modified:   TestProyectoObligatorio/TestProyectoObligatorio/UnitTest1.cs

--- TestProyectoObligatorio/TestProyectoObligatorio/UnitTest1.cs
+++ TestProyectoObligatorio/TestProyectoObligatorio/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace TestProyectoObligatorio
         public void TestMethod1()
         {
             SistemFilesOpener TestFileSistemOpener = new SistemFilesOpener();
+            TestFileSistemOpener.FileOpener("C:\Test");
 
         }
     }
