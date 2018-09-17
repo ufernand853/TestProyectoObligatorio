bf41a82b935916bf5ab67da7fa02ceb26766ea94
Ulifer853 <ufernand853@gmail.com>
2018-09-17 08:32:10 -0300

    new file:   TestProyectoObligatorio/TestProyectoObligatorio.sln 	new file:   TestProyectoObligatorio/TestProyectoObligatorio/IOpener.cs 	new file:   TestProyectoObligatorio/TestProyectoObligatorio/Properties/AssemblyInfo.cs 	new file:   TestProyectoObligatorio/TestProyectoObligatorio/SistemFilesOpener.cs 	new file:   TestProyectoObligatorio/TestProyectoObligatorio/TestProyectoObligatorio.csproj 	new file:   TestProyectoObligatorio/TestProyectoObligatorio/UnitTest1.cs

---
+++ TestProyectoObligatorio/TestProyectoObligatorio/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestProyectoObligatorio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestProyectoObligatorio")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de8d71cc-71ce-4489-ab78-c6a33bbb1b4b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestProyectoObligatorio/TestProyectoObligatorio/SistemFilesOpener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProyectoObligatorio
+{
+    class SistemFilesOpener : IOpener
+
+    {
+        public void FileOpener(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
